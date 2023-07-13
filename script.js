7f4490a1c838205eb45de57f8cf268ef56b44c33
RYeeAnn <ryeean16@gmail.com>
2023-07-12 17:08:24 -0700

    sprint 2 javascript test

---
+++ script.js
@@ -0,0 +1,60 @@
+let conversationComments = [{
+    name: "Conner Walton",
+    date: "02/17/2021",
+    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+},
+
+{
+    name: "Emilie Beach",
+    date: "01/09/2021",
+    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+},
+
+{
+    name: "Miles Acosta",
+    date: "12/20/2020",
+    comment: "I can't stop listening. Every time I ear one of their songs - the vocals - it gives me goosebumps. Shiveres straight down my spine. What a beautiful expression of creativity. Can't get enough."
+}
+]
+    
+    // function commentDisplay(comment, commentContainer) {
+    //     console.log(comment.name);
+    //     let comment__row = document.createElement('div');
+    //     comment__row.classList.add('conversation__row');
+    //     commentContainer.appendChild(comment__row);
+
+    //     let comment__column = document.createElement('div');
+    //     comment__column.classList.add('converesation__column');
+
+    //     let comments__card = document.createElement('div');
+    //     comments__card.classList.add('conversation__card');
+
+    //     let comments__img = document.createElement('img');
+    //     comments__img.classList.add('conversation__img');
+
+    //     let comments__name = document.createElement('p');
+    //     comments__name.classList.add('conversation__name');
+    //     comments__name.innerText = comment.name;
+    //     comment__row.appendChild(comments__name);
+
+    //     let comments__date = document.createElement('p');
+    //     comments__date.classList.add('conversation__date');
+
+    //     let comments__text = document.createElement('p');
+    //     comments__text.classList.add('conversation__text');
+
+    // }
+
+    // let comments = document.querySelector('.conversation__comments'); 
+
+    // for (let i = 0; i < conversationComments.length; i++) {
+    //     commentDisplay(conversationComments[i], comments);
+    // }
+
+
+
+
+
+
+
+
