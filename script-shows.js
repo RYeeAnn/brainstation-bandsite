8a2d9ddfacbeaf537e10f58864913e8b5dc2e569
RYeeAnn <ryeean16@gmail.com>
2023-07-17 03:21:40 -0700

    sprint 3 start

--- script-shows.js
+++ script-shows.js
@@ -1,81 +1,110 @@
-//Shows
-let showsInformation = [{
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA"
-},
-{
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA"
-},
-{
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA"
-},
-{
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA"
-},
-{
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA"
-},
-{
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA"
-},
+let shows__array = [
+    {
+        "date": "Mon Sept 06 2021",
+        "venue": "Ronald Lane",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Tue Sept 21 2021",
+        "venue": "Pier 3 East",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Fri Oct 15 2021",
+        "venue": "View Lounge",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Sat Nov 06 2021",
+        "venue": "Hyatt Agency",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Fri Nov 26 2021",
+        "venue": "Moscow Center",
+        "location": "San Francisco, CA"
+    },
+
+    {
+        "date": "Wed Dec 15 2021",
+        "venue": "Press Club",
+        "location": "San Francisco, CA"
+    }
 ];
 
-//Parent
-let showsList = document.querySelector(".shows"); 
 
-for (let i = 0; i < showsInformation.length; i++) {
-    let shows = showsInformation[i];
+//For tablet and desktop ONLY
+let table = document.querySelector(".shows__table");
 
-    let shows__item = document.createElement("ul");
-    shows__item.classList.add("shows__item");
+let main__header = document.createElement("div");
+main__header.className = "shows__header";
 
-    let shows__dateTitle = document.createElement("li");
-    shows__dateTitle.classList.add("shows__date--title");
-    shows__dateTitle.innerText = "DATE";
-    shows__item.appendChild(shows__dateTitle);
+let header__dates = document.createElement("p");
+header__dates.innerText = "DATES";
+header__dates.className = "shows__head";
 
-    let shows__date = document.createElement("li");
-    shows__date.classList.add("shows__date");
-    shows__date.innerText = shows.date;
-    shows__item.appendChild(shows__date);
+let header__venue = document.createElement("p");
+header__venue.innerText = "VENUE";
+header__venue.className = "shows__head";
 
-    let shows__venueTitle = document.createElement("li");
-    shows__venueTitle.classList.add("shows__venue--title");
-    shows__venueTitle.innerText = "VENUE";
-    shows__item.appendChild(shows__venueTitle);
+let header__location = document.createElement("p");
+header__location.innerText = "LOCATION";
+header__location.className = "shows__head";
 
-    let shows__venue = document.createElement("li");
-    shows__venue.classList.add("shows__venue");
-    shows__venue.innerText = shows.venue;
-    shows__item.appendChild(shows__venue);
+main__header.appendChild(header__dates);
+main__header.appendChild(header__venue);
+main__header.appendChild(header__location);
+table.appendChild(main__header);
 
-    let shows__locationTitle = document.createElement("li");
-    shows__locationTitle.classList.add("shows__location--title");
-    shows__locationTitle.innerText = "LOCATION";
-    shows__item.appendChild(shows__locationTitle);
 
-    let shows__location = document.createElement("li");
-    shows__location.classList.add("shows__location");
-    shows__location.innerText = shows.location;
-    shows__item.appendChild(shows__location);
 
-    //button
+
+
+
+
+function displayShow(show) {
+    let shows__card = document.createElement("div");
+    shows__card.className = "shows__card";
+
+    let shows__dateText = document.createElement("p");
+    shows__dateText.innerText = "DATE";
+    shows__dateText.className = "shows__heading";
+    shows__card.appendChild(shows__dateText);
+
+    let shows__date = document.createElement("h3");
+    shows__date.innerText = show["date"];
+    shows__card.appendChild(shows__date);
+
+    let shows__venueText = document.createElement("p");
+    shows__venueText.innerText = "VENUE";
+    shows__venueText.className = "shows__heading";
+    shows__card.appendChild(shows__venueText);
+
+    let shows__venue = document.createElement("p");
+    shows__venue.innerText = show["venue"];
+    shows__card.appendChild(shows__venue);
+
+    let shows__locationText = document.createElement("p");
+    shows__locationText.innerText = "LOCATION";
+    shows__locationText.className = "shows__heading";
+    shows__card.appendChild(shows__locationText);
+
+    let shows__location = document.createElement("p");
+    shows__location.innerText = show["location"];
+    shows__card.appendChild(shows__location);
+
+    document.querySelector(".shows__table").appendChild(shows__card);
+
     let shows__button = document.createElement("button");
-    shows__button.classList.add("shows__button");
     shows__button.innerText = "BUY TICKETS";
-    shows__item.appendChild(shows__button);
+    shows__button.classList.add("shows__button");
+    shows__card.appendChild(shows__button);
+}
 
-    showsList.appendChild(shows__item)
-
+for (let i = 0; i < shows__array.length; i++) {
+    displayShow(shows__array[i]);
 }
